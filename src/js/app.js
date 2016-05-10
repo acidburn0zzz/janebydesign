1bc996495c06623c0fa98ab64d7eaf709772f88b
Jane <jglundgren14@gmail.com>
2016-05-10 11:35:57 -0700

    styling and js update

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
 console.log('hello!');
+
+// Brand image fade-in text on hover
+$(".navbar-brand").hover(
+    function() {
+      $(this).append( $("<span> Back to home</span>") ).fadeIn( 500 );
+    }, function () {
+      $(this).find("span:last").remove();
+    }
+  );
